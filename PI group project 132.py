08ee46d40c97b85d48f2fca143890813fbd18bce
IanDuncan464 <71780837+IanDuncan464@users.noreply.github.com>
2020-10-19 11:58:37 -0500

    Add files via upload

--- PI group project 132.py
+++ PI group project 132.py
@@ -53,6 +53,15 @@ while running:
         elif event.type == QUIT:
             running = False
 
+    #getting the key that is currently being pressed
+    pressed_keys = pygame.key.get_pressed()
+    #updating player sprite due to the user pressing the keys
+    player.update(pressed_keys)
+
+    #screen filled with black
+    screen.fill((0, 0, 0))
+    
+
 #setting the screen white
 screen.fill((255,255,255))
 
@@ -61,6 +70,49 @@ surf = pygame.Surface((50, 50))
 #giving the surface a color so it can be separate from the background
 surf.fill((0, 0, 0))
 rect = surf.get.rect()
+
+#making the player object by using pygmae.sprite.Sprite
+#This allows us to make the game how we want i.e. making the player start on one side of the
+#screena and also have the attacks come from the other side of the screen
+class Player(pygame.sprite.Sprite):
+    super(Player, self).__init__()
+    self.surf = pygame.Surface((75, 25))
+    self.surf.fill((255, 255, 255))
+    self.rect = self.surf.get_rect()
+
+#filling the screen black
+screen.fill((0,0,0))
+
+#Having the player drawn on the screen
+screen.blit(player.surf, player.rect)
+#updating the display for the game
+pygame.display.flip()
+
+
+#setting up the keys for the user to use for the game
+pressed_keys = pygame.key.get_pressed()
+#moving the sprite from the keys that are pressed by the user
+def update(self, pressed_keys):
+    if pressed_keys[K_UP]:
+        self.rect.move_ip(0, -5)
+    if pressed_keys[K_DOWN]:
+        self.rect.move_ip(0, 5)
+    if pressed_keys[K_LEFT]:
+        self.rect.move_ip(-5, 0)
+    if pressed_keys[K_RIGHT]:
+        self.rect.move_ip(5, 0)
+
+    #keeping the player from going out of bounds
+    if self.rect.left < 0:
+        self.rect.left = 0
+    if self.rect.right > SCREEN_WIDTH:
+        self.rect.right = SCREEN_WIDTH
+    if self.rect.top <= 0:
+        self.rect.top = 0
+    if self.rect.bottom >= SCREEN_HEIGHT:
+        self.rect.bottom = SCREEN_HEIGHT
+
+    
 
 
 
